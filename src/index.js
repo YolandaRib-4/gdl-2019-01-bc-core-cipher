854ad7a2b40435b64d23f1b86f916983054a63dd
Yolanda Ribas <yolanda.ribas@hotmail.com>
2019-01-31 23:07:45 -0600

    add the encrypt function and show the decrypted message in the document

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let idPantallaActual = "pantalla-bienvenida";
 let pantallaInicial = document.getElementById(idPantallaActual);
 pantallaInicial.style.display = "block";
 
+
 // Funcion para controlar pantallas
 const controladorPantallas = (idPantallaNueva) => { 
   const pantallaActual = document.getElementById(idPantallaActual);
@@ -11,3 +12,22 @@ const controladorPantallas = (idPantallaNueva) => {
   pantallaNueva.style.display = "block";
   idPantallaActual = idPantallaNueva;
 }
+
+//Funcion para guardar el mensaje - offset e imprimir
+const input = () => {
+let mensaje = document.getElementById("mensaje").value;
+let offset = parseInt(document.getElementById("offset").value);
+  
+ let mensajeEncriptado = [];
+  
+for (let recorrido = 0 ; recorrido < mensaje.length ; recorrido++) {   
+ let enascii = mensaje.charCodeAt(recorrido);
+ let nuevaposicion = (enascii - 65 + offset)%26 + 65;
+ let nuevaletra = String.fromCharCode(nuevaposicion);
+ mensajeEncriptado.push(nuevaletra);
+}  
+  
+ const paraimprimirmensaje = document.getElementById("imprimir"); 
+ paraimprimirmensaje.innerHTML = mensajeEncriptado.join(''); 
+  
+ }
