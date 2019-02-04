ced2d64e7333b838d67fc8886d9b7fb3072bec4c
Yolanda Ribas <yolanda.ribas@hotmail.com>
2019-02-04 16:57:30 -0600

    move the functions and reorganize the structure according to the requirements

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,33 @@
-window.cipher = {
-  // ... 
-};
+window.cipher = {};
+
+//Funcion ENCRIPTAR
+cipher.encode = (offset, mensaje) => {
+  
+    
+  let mensajeEncriptado = [];
+    
+  for (let recorrido = 0 ; recorrido < mensaje.length ; recorrido++) {   
+   let enascii = mensaje.charCodeAt(recorrido);
+   let nuevaposicion = (enascii - 65 + offset)%26 + 65;
+   let nuevaletra = String.fromCharCode(nuevaposicion);
+   mensajeEncriptado.push(nuevaletra);
+  }  
+  return mensajeEncriptado.join('');  
+   
+   };
+  
+  //Funcion DESCENCRIPTAR
+   cipher.decode = (offset, mensaje) => {
+    
+      
+    let mensajeEncriptado = [];
+      
+    for (let recorrido = 0 ; recorrido < mensaje.length ; recorrido++) {   
+     let enascii = mensaje.charCodeAt(recorrido);
+     let nuevaposicion = (enascii + 65 - offset)%26 + 65;
+     let nuevaletra = String.fromCharCode(nuevaposicion);
+     mensajeEncriptado.push(nuevaletra);
+    }  
+    return mensajeEncriptado.join(''); 
+     
+     };
--- src/index.js
+++ src/index.js
@@ -2,48 +2,49 @@
 let idPantallaActual = "pantalla-bienvenida";
 let pantallaInicial = document.getElementById(idPantallaActual);
 pantallaInicial.style.display = "block";
+const btn1 = document.getElementById('btn-1');
+const btn2 = document.getElementById('btn-2');
+const btn3 = document.getElementById('btn-3');
+const btnCifrar = document.getElementById('btn-cifrar');
+const btnDescifrar = document.getElementById('btn-descifrar');
+
+btn1.onclick = ()=> controladorPantallas('pantalla-bienvenida');
+btn2.onclick = ()=> controladorPantallas('pantalla-2');
+btn3.onclick = ()=> controladorPantallas('pantalla-3');
+btnCifrar.onclick = ()=> seleccionador(true);
+btnDescifrar.onclick = ()=> seleccionador(false);
 
 // Funcion para controlar pantallas
-const controladorPantallas = (idPantallaNueva) => { 
+const controladorPantallas = (idPantallaNueva) => {
   const pantallaActual = document.getElementById(idPantallaActual);
   const pantallaNueva = document.getElementById(idPantallaNueva); 
   pantallaActual.style.display = "none";
   pantallaNueva.style.display = "block";
   idPantallaActual = idPantallaNueva;
-}
+};
 
-//Funcion ENCRIPTAR
-const encode = () => {
-let mensaje = document.getElementById("mensaje").value;
-let offset = parseInt(document.getElementById("offset").value);
-  
-let mensajeEncriptado = [];
-  
-for (let recorrido = 0 ; recorrido < mensaje.length ; recorrido++) {   
- let enascii = mensaje.charCodeAt(recorrido);
- let nuevaposicion = (enascii - 65 + offset)%26 + 65;
- let nuevaletra = String.fromCharCode(nuevaposicion);
- mensajeEncriptado.push(nuevaletra);
-}  
-  
- const paraimprimirmensaje = document.getElementById("imprimir"); 
- paraimprimirmensaje.innerHTML = mensajeEncriptado.join('');   
- }
+const seleccionador = (encript) => {
+    if (encript) {
+      let mensaje = document.getElementById("mensaje").value;
+      let offset = parseInt(document.getElementById("offset").value);
+      mensaje = cipher.encode(offset, mensaje);
+      const paraimprimirmensaje = document.getElementById("imprimir"); 
+      paraimprimirmensaje.innerHTML = mensaje; 
+    } else {
+      let mensaje = document.getElementById("mensaje-descifrar").value;
+      let offset = parseInt(document.getElementById("offset-negativo").value);
+      mensaje = cipher.decode(offset, mensaje);
+      const paraimprimirmensaje = document.getElementById("imprimir-descifrado"); 
+      paraimprimirmensaje.innerHTML = mensaje;  
 
-//Funcion DESCENCRIPTAR
- const decode = () => {
-  let mensaje = document.getElementById("mensaje-descifrar").value;
-  let offset = parseInt(document.getElementById("offset-negativo").value);
-    
-  let mensajeEncriptado = [];
-    
-  for (let recorrido = 0 ; recorrido < mensaje.length ; recorrido++) {   
-   let enascii = mensaje.charCodeAt(recorrido);
-   let nuevaposicion = (enascii + 65 - offset)%26 + 65;
-   let nuevaletra = String.fromCharCode(nuevaposicion);
-   mensajeEncriptado.push(nuevaletra);
-  }  
-    
-   const paraimprimirmensaje = document.getElementById("imprimir-descifrado"); 
-   paraimprimirmensaje.innerHTML = mensajeEncriptado.join('');     
-   }
+    }
+};
+
+
+
+
+
+  
+
+
+   
